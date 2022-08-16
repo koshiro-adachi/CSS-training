8ec369013c19e348688d9cb3fc8d21805bead288
koshiro-adachi <107029087+koshiro-adachi@users.noreply.github.com>
2022-08-16 19:19:37 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div className="container">
+        <button className="btn">Button</button>
+        <br />
+        <button className="btn2 float">Button</button>
+        <br />
+        <button className="btn3 slide-bg">
+          <span></span>Button
+        </button>
+        <div className="divall div1">div1</div>
+        <div className="divall div2">div2</div>
+        <div className="divall div3">div3</div>
+        <div className="divall div4">div4</div>
+      </div>
+    </div>
+  );
+}
